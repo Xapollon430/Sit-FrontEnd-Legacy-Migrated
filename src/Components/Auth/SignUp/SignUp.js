745a214b12fe0dd035d0e44f9e692906e0c25743
Vehbi <Xapollon430@gmail.com>
2020-05-06 00:51:53 -0400

    post request from forms added

--- src/Components/Auth/SignUp/SignUp.js
+++ src/Components/Auth/SignUp/SignUp.js
@@ -12,7 +12,7 @@ const SignUp = () => {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify(signUpInfo), // body data type must match "Content-Type" header
+            body: JSON.stringify(signUpInfo),
         });
     };
 
