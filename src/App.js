f4b306e1ae15ffe59040ca9d5b8ec38a0a67dff7
Anakonda11 <Xapollon430@gmail.com>
2020-05-10 23:37:52 -0400

    adding 2nd page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React, { Fragment } from "react";
 import Landing from "./Components/Landing/Landing";
 import { Route } from "react-router-dom";
 
-let x = 10;
-
 const App = () => {
     return (
         <Fragment>
