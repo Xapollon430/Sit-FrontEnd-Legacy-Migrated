b62ccf49c2a700ea008120b803ef1dceac186b20
Anakonda11 <Xapollon430@gmail.com>
2020-05-15 02:58:14 -0400

    local usereducer modal state

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Navbar, Brand, Menu, Nav, EmptyDiv, Button } from "./HeaderCss";
+import { useResponsive } from "../../CustomHooks/Hooks";
+import { useDispatch, useSelector } from "react-redux";
+import {
+    changeIsRegisterModalOpen,
+    changeIsSignUpOpen,
+    changeIsLogInOpen,
+} from "../../Redux/actions";
+
+function Header() {
+    const state = useSelector((state) => state);
+    const dispatch = useDispatch();
+    const [showHamburger, isHamburgerOpen] = useResponsive();
+
+    const changeTab = (event) => {
+        dispatch(changeIsRegisterModalOpen(true));
+        event.target.getAttribute("name") === "login"
+            ? dispatch(changeIsLogInOpen(true))
+            : dispatch(changeIsSignUpOpen(true));
+    };
+
+    return (
+        <Navbar>
+            <Brand>Sit!</Brand>
+            <Menu onClick={showHamburger} className="fas fa-bars fa-3x"></Menu>
+            <Nav close={isHamburgerOpen}>
+                <Button size="small" variant="outlined">
+                    Find A Sitter
+                </Button>
+                <Button variant="outlined">Be A Sitter</Button>
+                <EmptyDiv />
+                <Button variant="outlined" name="login" onClick={changeTab}>
+                    Log In
+                </Button>
+                <Button variant="outlined" name="signup" onClick={changeTab}>
+                    Sign Up
+                </Button>
+            </Nav>
+        </Navbar>
+    );
+}
+
+export default Header;
--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Header from "./Header/Header";
+import React, { useReducer } from "react";
+import Header from "../Header/Header";
 import Jumbotron from "./Jumbotron/Jumbotron";
 import BackgroundImage from "./LandingCss";
 import Modal from "../../UI/Modal/Modal";
@@ -7,13 +7,28 @@ import AuthModal from "../Auth/AuthModal";
 import { useDispatch, useSelector } from "react-redux";
 import { changeIsRegisterModalOpen } from "../../Redux/actions";
 
+const initialReducerState = {
+    isModalOpen: false,
+    isLogInOpen: false,
+    isSignUpOpen: false,
+};
+
+const modalReducer = (state = initialReducerState, action) => {
+    switch (action.type) {
+        case "IS_MODAL_OPEN":
+            return {
+                ...state,
+            };
+    }
+};
+
 function Landing() {
-    let state = useSelector((state) => state);
-    let dispatch = useDispatch();
+    let [modalState, setModalState] = useReducer();
 
     const closeRegisterModal = () => {
         dispatch(changeIsRegisterModalOpen(false));
     };
+
     return (
         <BackgroundImage>
             <Header />
