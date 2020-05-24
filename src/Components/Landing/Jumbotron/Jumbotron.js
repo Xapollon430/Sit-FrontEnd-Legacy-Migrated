0682fb046ddad91118784814e3c31149cfff5337
Anakonda11 <Xapollon430@gmail.com>
2020-05-23 23:48:35 -0400

    Jumbotron usememo

--- src/Components/Landing/Jumbotron/Jumbotron.js
+++ src/Components/Landing/Jumbotron/Jumbotron.js
@@ -1,22 +1,30 @@
-import React from "react";
-import { Section, Title, Paragraph, Button, Footer, Link } from "./JumbotronCss";
+import React, { useMemo } from "react";
+import {
+    Section,
+    Title,
+    Paragraph,
+    Button,
+    Footer,
+    Link,
+} from "./JumbotronCss";
 
-function Jumbotron() {
-	return (
-		<Section>
-			<Title>
-				You love your pets, <br /> so do we.
-			</Title>
-			<Paragraph>
-				At Sit.com we provide the most quality <br />
-				caretakers to your beloved pets.
-			</Paragraph>
-			<Button>Find a Sitter!</Button>
-			<Footer>
-				Are you looking for a pet sitting job? <Link href="gugle">Apply Now.</Link>
-			</Footer>
-		</Section>
-	);
-}
+const Jumbotron = React.memo(() => {
+    return (
+        <Section>
+            <Title>
+                You love your pets, <br /> so do we.
+            </Title>
+            <Paragraph>
+                At Sit.com we provide the most quality <br />
+                caretakers to your beloved pets.
+            </Paragraph>
+            <Button>Find a Sitter!</Button>
+            <Footer>
+                Are you looking for a pet sitting job?{" "}
+                <Link href="gugle">Apply Now.</Link>
+            </Footer>
+        </Section>
+    );
+});
 
 export default Jumbotron;
